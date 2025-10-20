c8c74114588fe093ed9b4f50f79549c6d02fde9c
Kaiming <mads396@gmail.com>
2025-10-19 21:12:40 -0700

    update combine yaml

--- app/data/combine_yaml.py
+++ app/data/combine_yaml.py
@@ -4,12 +4,23 @@ import sys
 from pathlib import Path
 
 
+class LiteralString(str):
+    pass
+
+
+def literal_str_representer(dumper, data):
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+
+
+yaml.add_representer(LiteralString, literal_str_representer)
+
+
 def combine_yaml(input_dir, output_file):
     combined_data = []
 
     for file_path in input_dir.iterdir():
 
-        if file_path.suffix != '.yaml':
+        if file_path.suffix != ".yaml":
             continue
         print(file_path)
 
@@ -17,9 +28,24 @@ def combine_yaml(input_dir, output_file):
             data = yaml.safe_load(f)
             combined_data.extend(data)
 
-    print('#cards:', len(combined_data))
+    for i in combined_data:
+        if not i.get('details'):
+            continue
+
+        i.update({'details': LiteralString(i['details'])})
+
+
+    print("#cards:", len(combined_data))
     with open(output_file, "w", encoding="utf-8") as f:
-        yaml.safe_dump(combined_data, f, sort_keys=False, allow_unicode=True)
+        yaml.dump(
+            combined_data,
+            f,
+            default_flow_style=False,
+            allow_unicode=True,
+            sort_keys=False,
+            indent=2,
+            width=float("inf")
+        )
 
     print(f"Combined YAML saved to: {output_file}")
 
